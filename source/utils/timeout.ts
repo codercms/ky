a9703013a5f294aa6708883685235b4198a07b98
Dmitry K <coder1994@gmail.com>
2023-06-11 04:50:06 +0300

    Fix compatibility with SvelteKit
    Not sure why but SvelteKit not just depends on request headers, it seems like it hashes entire request init

--- source/utils/timeout.ts
+++ source/utils/timeout.ts
@@ -26,20 +26,7 @@ export default async function timeout(
 		})
 
 		void options
-			.fetch(request.url, {
-				body: request.body,
-				cache: request.cache,
-				credentials: request.credentials,
-				headers: request.headers,
-				integrity: request.integrity,
-				keepalive: request.keepalive,
-				method: request.method,
-				mode: request.mode,
-				redirect: request.redirect,
-				referrer: request.referrer,
-				referrerPolicy: request.referrerPolicy,
-				signal: request.signal
-			})
+			.fetch(request.url, {...request})
 			.then(resolve)
 			.catch(reject)
 			.then(() => {
